8aecfba1deab77f34f756a24805a62761846a3bf
Rishabh Sundli <sundlirishabh18@gmail.comgit config --global user.email sundlirishabh18@gmail.com>
2025-09-11 16:32:48 +0530

    Updated some flaws like:- Now nobody can withdraw or deposit negative amount

--- BankAccountManagementSystem.java
+++ BankAccountManagementSystem.java
@@ -3,27 +3,47 @@ package Bank_Account_Management_System;
 class BankAccount {
     String accountHolderName;
     double balance;
-    boolean isActive=true;
+    boolean isActive;
+
+    void updateStatus(){
+        if(balance<=0){
+        isActive=false;
+    }else{
+        isActive=true;
+    }
+    }
     
     BankAccount(String name,double bal){
         accountHolderName= name;
         balance=bal;
+        updateStatus();
     }
 
     double deposit(double amount){
+        if(amount<=0){
+            System.out.println("Deposit failed: Amount must be positive.");
+            return balance;
+        }
         balance+=amount;
+        updateStatus();
         System.out.println("Deposited: "+amount+" .New Balance: "+balance);
         return balance;
     }
 
     double withdraw(double amount){
+        if(amount<=0){
+            System.out.println("Withdraw failed: Amount must be positive.");
+            return balance;
+        }
         if(isActive&&amount<=balance){
         balance-=amount;
+        updateStatus();
         return balance;
     }else{
         System.out.println("Withdrawal failed: Account inactive or insufficient funds.");
-        return balance;
+        
     }
+    return balance;
     }   
 
     void displayInfo(){
